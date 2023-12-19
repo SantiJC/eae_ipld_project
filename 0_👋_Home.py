3a789bb8c93828fccb04221e23fa4c731a46ff05
Santi Jerez <santijc21101@gmail.com>
2023-12-19 18:21:26 +0100

    Completed the home page

--- 0_👋_Home.py
+++ 0_👋_Home.py
@@ -4,7 +4,7 @@ import base64
     
 # ----- Page configs (tab title, favicon) -----
 st.set_page_config(
-    page_title="<Your Name> Portfolio",
+    page_title="Santiago Jerez Portfolio",
     page_icon="📊",
 )
 
@@ -15,16 +15,16 @@ with st.sidebar:
     st.header("Introduction to Programming Languages for Data")
     st.write("###")
     st.write("***Final Project - Dec 2023***")
-    st.write("**Author:** <Your Name>")
+    st.write("**Author:** Santiago Jerez")
     st.write("**Instructor:** [Enric Domingo](https://github.com/enricd)")
 
 
 # ----- Top title -----
-st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is ????</h1></div>""", unsafe_allow_html=True)  # TODO: Add your name
+st.write(f"""<div style="text-align: center;"><h1 style="text-align: center;">👋 Hi! My name is Santiago Jerez</h1></div>""", unsafe_allow_html=True)  # TODO: Add your name
 
 
 # ----- Profile image file -----
-profile_image_file_path = "profile_image.png"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
+profile_image_file_path = "Foto cv.jpeg"       # TODO: Upload your profile image to the same folder as this script and update this if it has a different name
 
 with open(profile_image_file_path, "rb") as img_file:
     img = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
@@ -39,7 +39,7 @@ st.write(f"""
 
 
 # ----- Personal title or short description -----
-current_role = "Current Role and/or Studies or Description"   # TODO: Change this
+current_role = "Passionate about data analysis and with a solid background in Business Administration and Management, I have dedicated my career to exploring the world of Big Data and Data Analytics. I have pursued my career and my current Master's in Big Data and Data Analytics in English, which has provided me with a high level of competence in this language, essential in the global business and technology field."   # TODO: Change this
 
 st.write(f"""<div style="text-align: center;"><h4><i>{current_role}</i></h4></div>""", unsafe_allow_html=True)
 
@@ -51,17 +51,19 @@ st.subheader("About Me")
 
 # TODO: Modify and adapt the following lines to your info, you can add or remove some details if you want
 st.write("""
-- 🧑‍💻 I am a <Current Role and/or Studies> 
+- 🧑‍💻 I am a Data Analyst with a strategic profile
 
-- 🛩️ prev: <Previous Experience, Background or Studies>
+- 🛩️ prev: Data Analys intern in Heretat Oller del Mas and final grade project focused on Data Analytics
 
-- ❤️ <Your Passion and/or Interests>
+- ❤️ Data Science, Statistics, Programming, Investing and Managing
 
-- 🤖 <Your Personal Projects>
+- 🤖 Bot for identifying current real estate opportunities in Barcelona and Madrid
 
-- 🏂 <Your Hobbies>
+- 🏂 Investing in the stock market, crypto market, program tools, manage an online store
 
-- 📫 How to reach me: <Your Email>
+- 📫 How to reach me: SantiJC21101@gmail.com
+
+- 💼 Linkedin URL: https://www.linkedin.com/in/santiago-jerez-castro/
 
 - 🏠 Barcelona
 """)
